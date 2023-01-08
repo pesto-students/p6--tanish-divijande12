97eca3ea27b991aa66a0b1c97ba265901eb60d05
divijande12 <diviande@rocketmail.com>
2023-01-08 23:25:54 +0530

    Code changes

--- Week-10/url-shortner/src/components/layout/left.js
+++ Week-10/url-shortner/src/components/layout/left.js
@@ -7,13 +7,10 @@ const LeftContent = ({ value, setValue, errorMsg, handleEvent }) => {
         <div class="left-content">
             <div class="url-shortner-wrapper">
                 <div class="url-shotner">
-                    <span><i></i> Enter a long URL to make a TinyURL</span>
+                    <span><i></i> Enter long URL to make it short URL</span>
                     <Input type="text" cls="input" value={value} setValue={setValue} />
                     {errorMsg ? <span style={{ "color": "red", marginBottom: "5px", "width": "100%" }}>{errorMsg}</span> : ""}
-                    <Button text="Make URL Short!" cls="btn-primary" handleEvent={handleEvent} />
-                </div>
-                <div class="url-subtext">
-                    <p>By clicking Make Long URL Short, I agree to the Terms of Service, Privacy policy and Use of Cookies.</p>
+                    <Button text="Short it!" cls="btn-primary" handleEvent={handleEvent} />
                 </div>
             </div>
         </div>
